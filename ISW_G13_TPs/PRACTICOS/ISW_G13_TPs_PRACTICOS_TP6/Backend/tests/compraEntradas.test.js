3fdae8906cd5ff626c5b0d04e6e15e7c9f10161f
fabrivalseg <96061@sistemas.frc.utn.edu.ar>
2025-10-17 22:05:14 -0300

    add: Test prueba compra entrada fecha anterior a la actual

--- ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Backend/tests/compraEntradas.test.js
+++ ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Backend/tests/compraEntradas.test.js
@@ -1,15 +1,15 @@
-import comprarEntradas from "../src/compraEntradas.js";
+import { comprarEntradas } from "../src/comprarEntradas.js";
 import { describe, expect, test } from "@jest/globals";
 
 describe("Pruebas para la función compraEntradas", () => {
   test("Compra Valida con tarjeta y fecha correcta", async () => {
     const resultado = await comprarEntradas({
-      fecha: "2025-12-16",
+      fecha: "2025-10-21",
       cantidad: 3,
       edades: [25, 30, 12],
       tipoPase: "VIP",
       formaPago: "tarjeta",
-      email: "usuario@test.com",
+      email: "carla.gomez@hotmail.com",
     });
 
     expect(resultado.exito).toBe(true);
@@ -19,14 +19,27 @@ describe("Pruebas para la función compraEntradas", () => {
   test("Falla si no se selecciona forma de pago", async () => {
     await expect(
       comprarEntradas({
-        fecha: "2025-10-20",
+        fecha: "2025-10-21",
         cantidad: 3,
         edades: [25, 30, 12],
         tipoPase: "regular",
         formaPago: "",
-        email: "usuario@test.com",
+        email: "carla.gomez@hotmail.com",
       })
     ).rejects.toThrow("Debe seleccionar una forma de pago");
+  });
+
+  test("Falla si se intentan comprar más de 10 entradas", async () => {
+    await expect(
+      comprarEntradas({
+        cantidad: 11,
+        fecha: "2025-10-21",
+        edades: [25, 30, 12, 22, 23, 24, 25, 26, 27, 28, 29],
+        tipoPase: "VIP",
+        formaPago: "efectivo",
+        email: "carla.gomez@hotmail.com",
+      })
+    ).rejects.toThrow("La cantidad de entradas no puede ser mayor que 10.");
   });
 
   test("Falla si el parque está cerrado (Lunes)", async () => {
@@ -37,21 +50,51 @@ describe("Pruebas para la función compraEntradas", () => {
         edades: [20, 22],
         tipoPase: "regular",
         formaPago: "efectivo",
-        email: "usuario@test.com",
+        email: "carla.gomez@hotmail.com",
       })
     ).rejects.toThrow("El parque está cerrado ese día");
   });
 
-  test("Falla si se intentan comprar más de 10 entradas", async () => {
+  test("Falla si el parque está cerrado (Navidad)", async () => {
     await expect(
       comprarEntradas({
-        cantidad: 11,
-        fecha: "2025-12-16",
-        edades: [25, 30, 12, 22, 23, 24, 25, 26, 27, 28, 29],
-        tipoPase: "VIP",
+        fecha: "2025-12-25",
+        cantidad: 2,
+        edades: [20, 22],
+        tipoPase: "regular",
         formaPago: "efectivo",
-        email: "usuario@test.com",
+        email: "carla.gomez@hotmail.com",
       })
-    ).rejects.toThrow("La cantidad de entradas no puede ser mayor que 10.");
+    ).rejects.toThrow("El parque está cerrado ese día");
   });
+
+  test("Falla si el parque está cerrado (Año Nuevo)", async () => {
+    await expect(
+      comprarEntradas({
+        fecha: "2026-01-01",
+        cantidad: 4,
+        edades: [30, 32, 5, 8],
+        tipoPase: "VIP",
+        formaPago: "tarjeta",
+        email: "carla.gomez@hotmail.com",
+      })
+    ).rejects.toThrow("El parque está cerrado ese día");
+  });
+
+  test("Debe fallar si la fecha ingresada es anterior a la actual", async () => {
+    const fechaPasada = new Date();
+    fechaPasada.setDate(fechaPasada.getDate() - 1);
+
+    await expect(
+      comprarEntradas({
+        fecha: fechaPasada.toISOString().split("T")[0],
+        cantidad: 2,
+        edades: [20, 25],
+        tipoPase: "regular",
+        formaPago: "efectivo",
+        email: "carla.gomez@hotmail.com",
+      })
+    ).rejects.toThrow("La fecha no puede ser pasada");
+  });
+
 });
