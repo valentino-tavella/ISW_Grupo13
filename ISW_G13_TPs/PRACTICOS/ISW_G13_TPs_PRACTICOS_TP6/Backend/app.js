26e6144011fb2308c06e69d03dcbdcc6fb0f1cac
Fede-Coder <federiconieto04@gmail.com>
2025-10-21 20:01:10 -0300

    add: configuración de CORS y nuevo usuario en el mock de usuario

--- ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Backend/app.js
+++ ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Backend/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 import dbInit from "./db/db-init.js";
 import router from "./routes/comprarEntradas.router.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 
 app.use(express.json());
 app.use(router);
