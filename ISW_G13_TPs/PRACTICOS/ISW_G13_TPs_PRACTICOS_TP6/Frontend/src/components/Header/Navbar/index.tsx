71ade2f1b2a5b3fe1319a2603e21fa982bbd0ebd
Fede-Coder <federiconieto04@gmail.com>
2025-10-22 18:07:22 -0300

    remove: eliminación del Link en Navbar

--- ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Frontend/src/components/Header/Navbar/index.tsx
+++ ISW_G13_TPs/PRACTICOS/ISW_G13_TPs_PRACTICOS_TP6/Frontend/src/components/Header/Navbar/index.tsx
@@ -1,11 +1,6 @@
-import { Link } from "react-router-dom";
-
 function Navbar() {
     return (
         <>
-            <Link to="/" className="bg-india-green px-2 py-1 rounded-md text-white font-bold transition hover:bg-pigment-green">
-                <button className="primary" >Inicio</button>
-            </Link>
         </>
     );
 }
